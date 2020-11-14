96fb196f2dcf158fdbb22c20ebccc3538e5d918d
Kash15if <kash15if@yahoo.com>
2020-11-14 12:39:24 +0530

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,63 @@
+<template>
+ <div class="row">
+
+      <div class="col-md-3 " v-for="(repo,index) in repos" :key="index">
+      <a v-bind:href=repo.html_url>
+        <div class="card" >
+          <div class="card-body">
+            <h5 class="card-title" >{{repo.name}}</h5>
+            <p class="card-text">{{repo.description}}</p>
+            <h6 class="card-subtitle mb-2 text-muted">related topics</h6>
+            <p class="card-text">stars {{index}}   <span class="float-right">pull req</span></p>
+            
+            <div class="text-center">
+              <a v-bind:href=repo.homepage class="btn btn-primary" style="width: 60%; margin-top: 20px;">website</a>
+            </div>
+            
+          </div>
+        </div>
+        </a>
+        
+      </div>
+
+
+    </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  data(){
+    return {
+      repos: {}
+    };
+  },
+  async created(){
+      
+      const response = await fetch('https://api.github.com/users/Kash15if/repos')
+      
+      this.repos = await response.json();
+      await console.log(this.repos);
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .btn{
+        background-color: rgb(59, 17, 128);
+        color: white;
+        width: auto;
+        
+    }
+    a{
+      text-decoration: none;
+      color:#263238;
+    }
+    .card{
+      margin-top: 25px;
+    }
+</style>
